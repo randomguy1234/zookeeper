9d8ef16d3936361f2a99e1f9b760053aebef8eaa
Jimmy Rando <jsturat46925@gmail.com>
2022-02-09 15:30:51 -0500

    added another function and search query

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT= process.env.PORT || 3001;
 
 const app= express();
 
+
+//function for filtering out data in the animal.json file
 function filterByQuery(query, animalsArray)
 {
     let personalityTraitsArray= [];
@@ -57,6 +59,12 @@ function filterByQuery(query, animalsArray)
     return filteredResults;
 }
 
+function findById(id, animalsArray)
+{
+    const result= animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) =>
 {
     let results= animals;
@@ -66,6 +74,20 @@ app.get('/api/animals', (req, res) =>
     }
     
     res.json(results);
+});
+
+
+app.get('/api/animals/:id', (req, res) => 
+{
+    const result= findById(req.params.id, animals);
+    if (result)
+    {
+        res.json(result);
+    }
+    else
+    {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => 
